5121165e76bdd80ebc6da41c05906af816ba48a2
rıfai kuçi <rfai4747@hotmail.com>
2019-05-27 01:55:02 +0300

    yazar bilgileri dialog ekranı eklendi

--- app/src/main/java/com/rifaikuci/yoklamasistemiwifi_bluetooth/MainActivity.java
+++ app/src/main/java/com/rifaikuci/yoklamasistemiwifi_bluetooth/MainActivity.java
@@ -86,6 +86,6 @@ public class MainActivity extends AppCompatActivity {
         mBuilder.setView(mView);
         AlertDialog dialog = mBuilder.create();
         dialog.show();
-        //eklendi
+        //eklendis
     }
 }
